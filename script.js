4dc570592cb37146718a03e377e0bcd7ffccf2e1
Bukilola <Arasanyin01@gmail.com>
2022-10-22 13:03:21 +0200

    my week 6 project updated

---
+++ script.js
@@ -0,0 +1,64 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let dayIndex = date.getDay();
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.main.wind
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function enterCity(city) {
+  let apiKey = "1266ad07b66517497b1acf79ea5a6a64";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  enterCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "1266ad07b66517497b1acf79ea5a6a64";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentTime = new Date();
+let dateElement = document.querySelector("#current-day");
+dateElement.innerHTML = formatDate(currentTime);
+
+let form = document.querySelector(".form-control");
+form.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+enterCity("Weisendorf");
